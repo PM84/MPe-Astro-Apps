37fb22890902e818c3c209850c3175e8ea2a0362
Peter Mayer <peter.mayer@isb.bayern.de>
2021-12-01 11:19:29 +0100

    Adding Overview Page

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+
+<body>
+    <a href="Cepheiden/light_curve.html">Cepheiden - Lichtkurven</a>
+    <a href="Cepheiden/M_P_Beziehung.html">Cepheiden - Periode Helligkeit Beziehung</a>
+    <a href="ClusterFitting/index.html">HDR - Entfernungsmodul von Sternhaufen ermitteln</a>
+    <a href="Supernova/light_curve.html">Supernovae - Lichtkurven</a>
+    <a href="Supernova/spectrum.html">Supernovae - Spektren</a>
+
+</body>
+
+
+</html>
